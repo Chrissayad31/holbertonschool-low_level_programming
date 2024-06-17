05ea2aad93845eedabb0580cdd087becdf796090
Chrissayad31 <9395@holbertonstudents.com>
2024-06-17 13:51:57 +0000

    hey

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /*
- * File: 4-puts.c
- * Auth: Chrissayad31 9395@holbertonschool.com>
+ * Fi 4-puts.c
+ * Auth: Chayad31 9395@holbertonschool.com>
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
+ * main - Printming is like building a multilingual
  *                puzzle, followed by a new line.
  *
  * Return: Always 0.
