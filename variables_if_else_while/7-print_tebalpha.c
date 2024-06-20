47891e36242f7c671176fd34b0c17cda0648df99
Chrissayad31 <9395@holbertonstudents.com>
2024-06-20 10:17:58 +0000

    lol

---
+++ variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (ok)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch => 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
