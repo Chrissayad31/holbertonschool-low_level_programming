5427c5cb1cfa5fec99f938c20771837ed96bd4f1
Chrissayad31 <9395@holbertonstudents.com>
2024-06-23 09:07:01 +0000

    sign if the number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *print_sign - the sign of a number
+ *@n: The number to be checked
+ *Return: return 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
