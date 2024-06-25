c9ff3b2e878ad958dde036cae0dd6ed00cae2ee0
Chrissayad31 <9395@holbertonstudents.com>
2024-06-25 15:12:48 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_square - function that prints a square
+ *@size: integer value
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+
+	}
+}
