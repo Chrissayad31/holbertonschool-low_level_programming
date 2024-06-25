04954a661e99e4da99ad15bbe5d0552e46f43523
Chrissayad31 <9395@holbertonstudents.com>
2024-06-25 14:26:33 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (num % 5 == 0)
-			printf("Buzz ");
-		else if (num == 100)
-			printf("Buzz");
+			printf(" Buzz");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
+		else if (num == 1)
+			printf ("%d", num);
 		else
-			printf("%d ", num);
+			printf(" %d", num);
 	}
 	return (0);
 }
