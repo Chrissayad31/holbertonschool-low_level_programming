7f3737a77e3e376218f3bb54f4109b18ef0eba2e
Chrissayad31 <9395@holbertonstudents.com>
2024-06-25 14:08:29 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 15 == 0)
+			printf("FizzBuzz ");
+		else if (num % 5 == 0)
+			printf("Buzz ");
+		else if (num % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", num);
+	}
+	return (0);
+}
