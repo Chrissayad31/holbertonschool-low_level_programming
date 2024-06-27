fb6acce52b38855c2f5e4993cf69de3ed843a29e
Chrissayad31 <9395@holbertonstudents.com>
2024-06-27 13:32:09 +0200

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#include "main.h"
 
-/**
-* print_rev - function that print a string in reverse
-* 
-* @str: integer value
-*/
-void print_rev(char *s)
-{
-         while (*s != '\0')
-         {
-                   _putchar(*str);
-                   str--;
-         }
-         _putchar('\n');
-}
