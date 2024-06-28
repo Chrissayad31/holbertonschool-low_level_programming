3d437f21bae66ab5083fa929d50d1f491e997bf0
Chrissayad31 <9395@holbertonstudents.com>
2024-06-28 11:49:50 +0000

    ok

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - function that prints half of a string
+ *
+ * @str: integer value
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i, start;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	/**
+	 * De©terminer le point dede©part pour l'impession
+	 */
+	if (length % 2 == 0)
+	{
+		start = length / 2; /** point de depart longueur paire */
+	}
+	else
+	{
+		start = (length - 1) / 2 + 1; /** point de depart longueur impaire */
+	}
+
+	/**
+	 * Imprimer les caracteres a partir de 'start' jusqu'a la fin de la chaine
+	 */
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	putchar('\n');
+}
