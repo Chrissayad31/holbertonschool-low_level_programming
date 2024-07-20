190b7a222e55286ba4dfcee2197061be9b66ec1e
Chrissayad31 <9395@holbertonstudents.com>
2024-07-20 18:15:44 +0000

    finish

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,25 +1,19 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_memset(char *s, char b, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif
