1c21b098bb629ca0faa20b5d9f120ff5b66b6adf
Chrissayad31 <9395@holbertonstudents.com>
2024-07-20 14:45:20 +0000

    ok

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_array - print n element of array
  *@a: array
@@ -8,6 +9,7 @@
 void print_array(int *a, int n)
 {
 	int inc;
+
 	for (inc = 0; inc < n ; inc++)
 	{
 		if (inc != n - 1)
