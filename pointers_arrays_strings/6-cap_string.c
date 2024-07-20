8a38b3078bb94b63b85016bfb8019c2d509f80f5
Chrissayad31 <9395@holbertonstudents.com>
2024-07-20 18:30:14 +0000

    finish

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,42 @@
 #include "main.h"
 /**
- *cap_string - function that capitalize first character of a word
- *@str: string to capitalize
- *Return:returns the capitalized string
+ * cap_string - function that capitalizes the first character of each word
+ * @str: string to capitalize
+ * Return: returns the capitalized string
  */
 char *cap_string(char *str)
 {
 	int index = 0;
+	int capitalize = 1; // Flag to indicate if the next character should be capitalized
 
-	while (str[++index])
+	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		if (capitalize && (str[index] >= 'a' && str[index] <= 'z'))
+		{
+			str[index] -= 32; // Convert to uppercase
+			capitalize = 0; // Reset flag after capitalizing
+		}
+		else if (str[index] == ' ' ||
+				str[index] == '\t' ||
+				str[index] == '\n' ||
+				str[index] == ',' ||
+				str[index] == ';' ||
+				str[index] == '.' ||
+				str[index] == '!' ||
+				str[index] == '?' ||
+				str[index] == '"' ||
+				str[index] == '(' ||
+				str[index] == ')' ||
+				str[index] == '{' ||
+				str[index] == '}')
+		{
+			capitalize = 1; // Set flag to capitalize the next character
+		}
+		else
+		{
+			capitalize = 0; // Do not capitalize if current character is not a separator
+		}
+		index++;
 	}
 	return (str);
 }
