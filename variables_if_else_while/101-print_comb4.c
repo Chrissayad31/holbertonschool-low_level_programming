77bc12cf8e764c52e3f8157081e21abc82e89eb7
Chrissayad31 <9395@holbertonstudents.com>
2024-07-25 12:54:19 +0000

    supp

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -8,26 +8,26 @@
 
 int main(void)
 {
-        int i, j, k;
+	int i, j, k;
 
-        for (i = 0; i < 10; i++)
-        {
-                for (j = i + 1; j < 10; j++)
-                {
-                        for (k = j + 1; k < 10; k++)
-                        {
-                                putchar('0' + i);
-                                putchar('0' + j);
-                                putchar('0' + k);
-                                if (i != 7 || j != 8 || k != 9)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
