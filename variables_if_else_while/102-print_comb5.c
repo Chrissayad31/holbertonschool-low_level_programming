9557e2ef3ad68e702c0934d76dcfcf4819b8ccbf
Chrissayad31 <9395@holbertonstudents.com>
2024-07-25 13:51:29 +0000

    yes

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 		{
 			putchar('0' + i / 10);
 			putchar('0' + i % 10);
-			putchar(' ');         
+			putchar(' ');
 			putchar('0' + j / 10);
 			putchar('0' + j % 10);
 			if (i != 98 || j != 99)
